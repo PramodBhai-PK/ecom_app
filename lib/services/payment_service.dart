0385b59e1a99d6727a0264f809569bd282fe04d1
PramodBhai-PK <pkyadav4488@gmail.com>
2020-07-15 19:31:46 +0530

    Changes in payment service

---
+++ lib/services/payment_service.dart
@@ -0,0 +1,14 @@
+import 'package:ecomapp/models/payment.dart';
+import 'package:ecomapp/repository/repository.dart';
+
+class PaymentService {
+  Repository _repository;
+
+  PaymentService(){
+    _repository = Repository();
+  
+  }
+  makePayment(Payment payment) async {
+    return await _repository.httpPost('make-payment', payment.toJson());
+  }
+}
